c1065c78e0d97a1ad2654472d56234479b5c3c06
Tanmay Shah <tanmay.shah@oslash.com>
2023-10-24 03:38:42 +0530

    fix: add lat long in server

--- server/index.js
+++ server/index.js
@@ -10,17 +10,17 @@ dotenv.config()
 const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY
 
 const OPEN_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
-const lat = '12.9719'
-const lon = '77.5937'
+// const lat = '12.9719'
+// const long = '77.5937'
 
-async function getWeatherForecastData() {
-  const weatherForecastDataResponse = await fetch(`${OPEN_WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`)
+async function getWeatherForecastData(lat, long) {
+  const weatherForecastDataResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover,windspeed_10m,winddirection_10m&timezone=auto`)
   const weatherForecastData = await weatherForecastDataResponse.json()
   return weatherForecastData
 }
 
 async function getWeatherData() {
-  const weatherDataResponse = await fetch(`${OPEN_WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`)
+  const weatherDataResponse = await fetch(`${OPEN_WEATHER_API_URL}/weather?lat=${lat}&lon=${long}&appid=${OPEN_WEATHER_API_KEY}&units=metric`)
   const weatherData = await weatherDataResponse.json()
   return weatherData
 }
@@ -39,12 +39,14 @@ app.ws('/weather', async function (ws, req) {
 })
 
 app.ws('/forecast', async function (ws, req) {
+  const latitude = req.query.lat
+  const longitude = req.query.long
   setInterval(async () => {
-    const weatherForecastData = await getWeatherForecastData()
+    const weatherForecastData = await getWeatherForecastData(latitude, longitude)
     ws.send(JSON.stringify(weatherForecastData))
   }, 30000)
 
-  const weatherForecastData = await getWeatherForecastData()
+  const weatherForecastData = await getWeatherForecastData(latitude, latitude)
   ws.send(JSON.stringify(weatherForecastData))
 
   ws.on('message', function message(data) {
