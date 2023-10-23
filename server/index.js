400779c2b4b68b9660c3701aa51606cfebaf52be
Tanmay Shah <tanmay.shah@oslash.com>
2023-10-24 04:43:58 +0530

    fix: lat long issue

--- server/index.js
+++ server/index.js
@@ -6,11 +6,8 @@ const app = express()
 expressWs(app)
 
 async function getWeatherForecastData(lat, long) {
-  console.log(lat)
-  console.log(long)
   const weatherForecastDataResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover,windspeed_10m,winddirection_10m&timezone=auto`)
   const weatherForecastData = await weatherForecastDataResponse.json()
-  console.log('weatherForecast', weatherForecastData)
   return weatherForecastData
 }
 
@@ -23,14 +20,12 @@ async function getWeatherForecastData(lat, long) {
 app.ws('/forecast', async function (ws, req) {
   const latitude = req.query.lat ?? 12
   const longitude = req.query.long ?? 27
-  console.log('>>>', latitude)
-  console.log('>>>', longitude)
   setInterval(async () => {
     const weatherForecastData = await getWeatherForecastData(latitude, longitude)
     ws.send(JSON.stringify(weatherForecastData))
   }, 1000 * 60 * 2)
 
-  const weatherForecastData = await getWeatherForecastData(latitude, latitude)
+  const weatherForecastData = await getWeatherForecastData(latitude, longitude)
   ws.send(JSON.stringify(weatherForecastData))
   ws.on('message', function message(data) {
     console.log('received: %s', data);
