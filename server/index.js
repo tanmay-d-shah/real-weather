44d7ca0de12ecbc36c6f0db8498cb9066ccc1922
Tanmay Shah <tanmay.shah@oslash.com>
2023-10-23 11:58:18 +0530

    feat: add express routes in server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,55 @@
-import { WebSocketServer } from 'ws';
 import fetch from 'node-fetch'
-const wss = new WebSocketServer({ port: 5173 });
+import dotenv from 'dotenv'
+import express from 'express'
+import expressWs from 'express-ws'
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+const app = express()
+expressWs(app)
+dotenv.config()
+
+const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY
+
+const OPEN_WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+const lat = '12.9719'
+const lon = '77.5937'
+
+async function getWeatherForecastData() {
+  const weatherForecastDataResponse = await fetch(`${OPEN_WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`)
+  const weatherForecastData = await weatherForecastDataResponse.json()
+  return weatherForecastData
+}
+
+async function getWeatherData() {
+  const weatherDataResponse = await fetch(`${OPEN_WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`)
+  const weatherData = await weatherDataResponse.json()
+  return weatherData
+}
+
+app.ws('/weather', async function (ws, req) {
+  setInterval(async () => {
+    const weatherData = await getWeatherData()
+    ws.send(JSON.stringify(weatherData))
+  }, 10000)
+
+  const weatherData = await getWeatherData()
+  ws.send(JSON.stringify(weatherData))
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+})
+
+app.ws('/forecast', async function (ws, req) {
+  setInterval(async () => {
+    const weatherForecastData = await getWeatherForecastData()
+    ws.send(JSON.stringify(weatherForecastData))
+  }, 30000)
+
+  const weatherForecastData = await getWeatherForecastData()
+  ws.send(JSON.stringify(weatherForecastData))
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);
   });
+})
 
-  setInterval(async () => {
-    const weatherDataResposen = await fetch('https://api.open-meteo.com/v1/forecast?latitude=12.9719&longitude=77.5937&hourly=temperature_2m,precipitation,windspeed_10m&timezone=auto')
-    const weatherData = await weatherDataResposen.json()
-    ws.send(JSON.stringify(weatherData))
-  }, 10000)
-});
+app.listen(5174)
