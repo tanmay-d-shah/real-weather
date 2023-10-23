847fe9f5738a3a5b76c95440538144da79738039
Tanmay Shah <tanmay.shah@oslash.com>
2023-10-24 04:15:43 +0530

    chore: server fixes

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
-// import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 // import dotenv from 'dotenv'
-// import express from 'express'
-// import expressWs from 'express-ws'
+import express from 'express'
+import expressWs from 'express-ws'
 
-import { WebSocketServer } from 'ws'
+// import { WebSocketServer } from 'ws'
 
-const wss = new WebSocketServer({ port: 5173 });
+// const wss = new WebSocketServer({ port: 5173 });
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+// wss.on('connection', function connection(ws) {
+//   ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
+//   ws.on('message', function message(data) {
+//     console.log('received: %s', data);
+//   });
 
-  ws.send('something');
-});
+//   ws.send('something');
+// });
 
-// const app = express()
-// expressWs(app)
+const app = express()
+expressWs(app)
 // dotenv.config()
 
 // const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY
@@ -27,11 +27,11 @@ wss.on('connection', function connection(ws) {
 // // const lat = '12.9719'
 // // const long = '77.5937'
 
-// async function getWeatherForecastData(lat, long) {
-//   const weatherForecastDataResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover,windspeed_10m,winddirection_10m&timezone=auto`)
-//   const weatherForecastData = await weatherForecastDataResponse.json()
-//   return weatherForecastData
-// }
+async function getWeatherForecastData(lat, long) {
+  const weatherForecastDataResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover,windspeed_10m,winddirection_10m&timezone=auto`)
+  const weatherForecastData = await weatherForecastDataResponse.json()
+  return weatherForecastData
+}
 
 // async function getWeatherData() {
 //   const weatherDataResponse = await fetch(`${OPEN_WEATHER_API_URL}/weather?lat=${lat}&lon=${long}&appid=${OPEN_WEATHER_API_KEY}&units=metric`)
@@ -39,21 +39,21 @@ wss.on('connection', function connection(ws) {
 //   return weatherData
 // }
 
-// app.ws('/forecast', async function (ws, req) {
-//   const latitude = req.query.lat ?? 12
-//   const longitude = req.query.long ?? 27
-//   setInterval(async () => {
-//     const weatherForecastData = await getWeatherForecastData(latitude, longitude)
-//     ws.send(JSON.stringify(weatherForecastData))
-//   }, 30000)
+app.ws('/forecast', async function (ws, req) {
+  const latitude = req.query.lat ?? 12
+  const longitude = req.query.long ?? 27
+  setInterval(async () => {
+    const weatherForecastData = await getWeatherForecastData(latitude, longitude)
+    ws.send(JSON.stringify(weatherForecastData))
+  }, 3000)
 
-//   const weatherForecastData = await getWeatherForecastData(latitude, latitude)
-//   ws.send(JSON.stringify(weatherForecastData))
+  const weatherForecastData = await getWeatherForecastData(latitude, latitude)
+  ws.send(JSON.stringify(weatherForecastData))
 
-//   ws.send('Hello')
-//   ws.on('message', function message(data) {
-//     console.log('received: %s', data);
-//   });
-// })
+  ws.send('Hello')
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+})
 
-// app.listen(5173)
+app.listen(5173)
