b93e9ed4ae685bf8c7a90c55f76af92a6c372c2e
Tanmay Shah <tanmay.shah@oslash.com>
2023-10-24 17:43:11 +0530

    feat: add current weather and average daily weather support

--- server/index.js
+++ server/index.js
@@ -5,31 +5,50 @@ import expressWs from 'express-ws'
 const app = express()
 expressWs(app)
 
-async function getWeatherForecastData(lat, long) {
-  const weatherForecastDataResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover,windspeed_10m,winddirection_10m&timezone=auto`)
+async function getHourlyWeatherForecast(lat, long) {
+  const weatherForecastDataResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,precipitation,cloudcover,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto`)
   const weatherForecastData = await weatherForecastDataResponse.json()
   return weatherForecastData
 }
 
-// async function getWeatherData() {
-//   const weatherDataResponse = await fetch(`${OPEN_WEATHER_API_URL}/weather?lat=${lat}&lon=${long}&appid=${OPEN_WEATHER_API_KEY}&units=metric`)
-//   const weatherData = await weatherDataResponse.json()
-//   return weatherData
-// }
+
+async function getCurrentWeatherData(lat, long) {
+  const currentWeatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m,relativehumidity_2m,precipitation,windspeed_10m&timezone=auto`)
+  const currentWeatherData = await currentWeatherResponse.json()
+  return currentWeatherData
+}
 
 app.ws('/forecast', async function (ws, req) {
   const latitude = req.query.lat ?? 12
   const longitude = req.query.long ?? 27
-  setInterval(async () => {
-    const weatherForecastData = await getWeatherForecastData(latitude, longitude)
-    ws.send(JSON.stringify(weatherForecastData))
-  }, 1000 * 60 * 2)
+  try {
+    setInterval(async () => {
+      const weatherForecastData = await getHourlyWeatherForecast(latitude, longitude)
+      ws.send(JSON.stringify(weatherForecastData))
+    }, 1000 * 60 * 2)
 
-  const weatherForecastData = await getWeatherForecastData(latitude, longitude)
-  ws.send(JSON.stringify(weatherForecastData))
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
+    const weatherForecastData = await getHourlyWeatherForecast(latitude, longitude)
+    ws.send(JSON.stringify(weatherForecastData))
+  } catch (e) {
+    console.log('Error: ', e)
+  }
+
 })
 
-app.listen(5173)
+app.ws('/current-weather', async function (ws, req) {
+  const latitude = req.query.lat ?? 12
+  const longitude = req.query.long ?? 27
+  try {
+    setInterval(async () => {
+      const currentWeatherData = await getCurrentWeatherData(latitude, longitude)
+      ws.send(JSON.stringify(currentWeatherData))
+    }, 1000 * 60 * 2)
+
+    const weatherForecastData = await getCurrentWeatherData(latitude, longitude)
+    ws.send(JSON.stringify(weatherForecastData))
+  } catch (e) {
+    console.log('Error: ', e)
+  }
+})
+
+app.listen(5174)
